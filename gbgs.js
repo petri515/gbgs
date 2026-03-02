f6bec7ca39077865ddf999e436c4c4d6ef59ca77
Juaraquer <juaraquer@unal.edu.co>
2026-03-01 22:45:10 -0500

    Create gbgs.js
    prueba chat gpt creacion de cuadricula inicial

---
+++ gbgs.js
@@ -0,0 +1,20 @@
+const matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+function setup() {
+  createCanvas(300, 300);
+  textAlign(CENTER+ 20, CENTER);
+  textSize(25);
+}
+
+
+function draw(){
+  
+    background(150)
+  square(mouseX-75,mouseY-75,150)
+  fill(0,0,0)
+
+}
